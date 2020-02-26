0be63f18c0745d072cece83e603f3f3ef0f4053c
Marina Pauw <pauwmarina@gmail.com>
2020-02-26 07:32:02 +0200

    Auto stash before merge of "master" and "origin/master"

--- UI_MainWindow.py
+++ UI_MainWindow.py
@@ -147,7 +147,7 @@ class Ui_MainWindow(QtWidgets.QTabWidget):
         Ui_MainWindow.EnableButtons(self)
         
     def checkColumnLength(self):
-       if(len(self.metrics.columns)<1):
+       if(len(Ui_MainWindow.metrics.columns)<1):
                 QMessageBox.warning(self.tab,"Error:" ,"After removing low variance columns, there were no columns left from which to conduct any sort of analysis.")
                 self.onBrowseClicked()
       
