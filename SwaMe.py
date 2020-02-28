2b6148f3aa84b28f5112abbd87f06a2edafa02e6
Marina Pauw <pauwmarina@gmail.com>
2020-02-28 16:53:03 +0200

    QFrames added - QuaMeter and SwaMe moved to mainwindow

--- SwaMe.py
+++ SwaMe.py
@@ -16,71 +16,7 @@ class SwaMe():
         SwaMe.QMWindow = QtWidgets.QWidget()
         SwaMe.QMWindow.setWindowTitle("SwaMe arguments")
         
-        #Widget declaring:
-        SwaMe.QMWindow.BrowseButton = QtWidgets.QPushButton(SwaMe.QMWindow)
-        SwaMe.QMWindow.files = QtWidgets.QLabel(SwaMe.QMWindow)
-        SwaMe.QMWindow.fileList = QtWidgets.QLabel(SwaMe.QMWindow)
-        SwaMe.QMWindow.divisionLabel= QtWidgets.QLabel(SwaMe.QMWindow)
-        SwaMe.QMWindow.divisionTextBox= QLineEdit(SwaMe.QMWindow)
-        SwaMe.QMWindow.MTLabel= QtWidgets.QLabel(SwaMe.QMWindow)
-        SwaMe.QMWindow.MTTextBox = QLineEdit(SwaMe.QMWindow)
-        SwaMe.QMWindow.RTLabel= QtWidgets.QLabel(SwaMe.QMWindow)
-        SwaMe.QMWindow.RTTextBox = QLineEdit(SwaMe.QMWindow)
-        #SwaMe.QMWindow.DirLabel = QtWidgets.QLabel(SwaMe.QMWindow)
-        SwaMe.QMWindow.Dir = QtWidgets.QRadioButton("Whole Directory:")
-        SwaMe.QMWindow.RUNButton = QtWidgets.QPushButton(SwaMe.QMWindow)
         
-        #Widget geometries:
-        SwaMe.QMWindow.files.setGeometry(QtCore.QRect(90, 120, 300, 20))
-        SwaMe.QMWindow.fileList.setGeometry(QtCore.QRect(90, 120, 300, 200))
-        SwaMe.QMWindow.divisionLabel.setGeometry(QtCore.QRect(90, 120, 300, 20))
-        SwaMe.QMWindow.MTLabel.setGeometry(QtCore.QRect(90, 120, 300, 20))
-        SwaMe.QMWindow.RTLabel.setGeometry(QtCore.QRect(90, 120, 300, 20))
-
-        #Widget stylesheets:
-        SwaMe.QMWindow.BrowseButton.setStyleSheet("background-color: rgb(240,240,240);")
-
-        #Widget texts:
-        SwaMe.QMWindow.BrowseButton.setText("Browse ")
-        SwaMe.QMWindow.files.setText("Files selected: ")
-        SwaMe.QMWindow.divisionLabel.setText("Number of segments to divide the RT into: ")
-        SwaMe.QMWindow.MTLabel.setText("MassTolerance:")
-        SwaMe.QMWindow.RTLabel.setText("RTTolerance:")
-        SwaMe.QMWindow.RUNButton.setText("RUN")
-
-        
-        #Layout:
-        SwaMe.QMWindow.vbox = QtWidgets.QVBoxLayout(SwaMe.QMWindow)
-        hbox1 = QtWidgets.QHBoxLayout()
-        hbox1.addWidget( SwaMe.QMWindow.BrowseButton)
-        vbox2 = QtWidgets.QVBoxLayout(SwaMe.QMWindow)
-        vbox2.addStretch()
-        vbox2.addWidget(SwaMe.QMWindow.files)     
-        vbox2.addWidget(SwaMe.QMWindow.fileList)
-        hbox1.addLayout(vbox2)
-        hbox1.addStretch()
-        SwaMe.QMWindow.vbox.addLayout(hbox1)
-        hbox2 = QtWidgets.QHBoxLayout()
-        hbox2.addWidget(SwaMe.QMWindow.divisionLabel)
-        hbox2.addWidget(SwaMe.QMWindow.divisionTextBox)
-        SwaMe.QMWindow.vbox.addLayout(hbox2)
-        hbox3 = QtWidgets.QHBoxLayout()
-        hbox3.addWidget(SwaMe.QMWindow.MTLabel)
-        hbox3.addWidget(SwaMe.QMWindow.MTTextBox)
-        hbox3.addWidget(SwaMe.QMWindow.RTLabel)
-        hbox3.addWidget(SwaMe.QMWindow.RTTextBox)
-        SwaMe.QMWindow.vbox.addLayout(hbox3)
-        hbox4 = QtWidgets.QHBoxLayout()
-        hbox4.addWidget(SwaMe.QMWindow.RUNButton)
-        SwaMe.QMWindow.vbox.addLayout(hbox4)
-        hbox5 = QtWidgets.QHBoxLayout()
-        hbox5.addWidget(SwaMe.QMWindow.RTLabel)
-        hbox5.addWidget(SwaMe.QMWindow.RTTextBox)
-        SwaMe.QMWindow.vbox.addLayout(hbox5)
-        hbox6 = QtWidgets.QHBoxLayout()
-        hbox6.addWidget(SwaMe.QMWindow.Dir)
-        SwaMe.QMWindow.vbox.addLayout(hbox6)
-        SwaMe.QMWindow.show()
 
        
         #Actions for when buttons are clicked:
