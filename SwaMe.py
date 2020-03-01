c2f1ebb8d88a9f3968d5523fcccca3b8ed26e76f
Marina Pauw <pauwmarina@gmail.com>
2020-03-01 21:50:13 +0200

    Reads the output from SwaMe to a textbox on mainwindow!

--- SwaMe.py
+++ SwaMe.py
@@ -12,27 +12,8 @@ import os
 class SwaMe():
     """description of class"""
     def setupUI(self, parent=None): 
-        #Setting up the window:
-        SwaMe.QMWindow = QtWidgets.QWidget()
-        SwaMe.QMWindow.setWindowTitle("SwaMe arguments")
-        
-        
+        UI_MainWindow.Ui_MainWindow.EnableSwaMeArguments(self)
 
-       
-        #Actions for when buttons are clicked:
-        SwaMe.QMWindow.BrowseButton.clicked.connect(SwaMe.onSwaMeBrowseClicked)
-        SwaMe.QMWindow.RUNButton.clicked.connect(SwaMe.onSwaMeRUNClicked)
-
-
-    def onSwaMeBrowseClicked(self):
-        #FileInput.BrowseWindow.__init__(FileInput.BrowseWindow, self)
-        SwaMe.QMWindow.files.show()
-        SwaMe.File = FileInput.BrowseWindow.GetSwaMeInputFile(SwaMe)
-        if(SwaMe.File):
-                fname = os.path.basename(SwaMe.File)
-                SwaMe.QMWindow.fileList.setText(fname)
-    
-    def onSwaMeRUNClicked(self):
         #Arguments:
         SwaMe.MassTolerance = 5000 #Arbitrary value that I hope no one would ever choose
         SwaMe.RTTolerance = 5000
@@ -40,20 +21,54 @@ class SwaMe():
         SwaMe.Dir=False
         #SwaMe.IRT==False
 
-        if(SwaMe.QMWindow.MTTextBox.text()):
-            SwaMe.MassTolerance = SwaMe.QMWindow.MTTextBox.text()
-        if(SwaMe.QMWindow.RTTextBox.text()):
-            SwaMe.RTTolerance = SwaMe.QMWindow.RTTextBox.text()
-        if(SwaMe.QMWindow.divisionTextBox.text()):
-            SwaMe.Division = SwaMe.QMWindow.divisionTextBox.text()
-        if(SwaMe.QMWindow.Dir.isChecked):
+        #Actions for when buttons are clicked:
+        UI_MainWindow.Ui_MainWindow.tab.UploadFrame.rightFrame.SBrowseButton.clicked.connect(SwaMe.onSwaMeBrowseClicked)
+        UI_MainWindow.Ui_MainWindow.tab.UploadFrame.rightFrame.SRUNButton.clicked.connect(SwaMe.onSwaMeRUNClicked)
+
+
+    def onSwaMeBrowseClicked(self):
+        #FileInput.BrowseWindow.__init__(FileInput.BrowseWindow, self)
+        UI_MainWindow.Ui_MainWindow.tab.UploadFrame.leftFrame.files.show()
+        SwaMe.File = FileInput.BrowseWindow.GetSwaMeInputFile(SwaMe)
+        if(SwaMe.File):
+                fname = os.path.basename(SwaMe.File)
+                UI_MainWindow.Ui_MainWindow.tab.UploadFrame.rightFrame.SfileList.setText(fname)
+    
+
+                
+    def onSwaMeRUNClicked(self):
+        SwaMePath = FileInput.BrowseWindow.GetSwaMePath(SwaMe)
+        SwaMe.process = QtCore.QProcess()
+        SwaMe.process.setProcessChannelMode(QProcess.MergedChannels)
+        SwaMe.process.readyReadStandardOutput.connect(lambda: SwaMe.on_readyReadStandardOutput(self))
+        arguments = ""
+        
+        if(UI_MainWindow.Ui_MainWindow.tab.UploadFrame.rightFrame.MTTextBox.text()):
+            SwaMe.MassTolerance = UI_MainWindow.Ui_MainWindow.tab.UploadFrame.rightFrame.MTTextBox.text()
+        if(UI_MainWindow.Ui_MainWindow.tab.UploadFrame.rightFrame.RTTextBox.text()):
+            SwaMe.RTTolerance = UI_MainWindow.Ui_MainWindow.tab.UploadFrame.rightFrame.RTTextBox.text()
+        if(UI_MainWindow.Ui_MainWindow.tab.UploadFrame.rightFrame.divisionTextBox.text()):
+            SwaMe.Division = UI_MainWindow.Ui_MainWindow.tab.UploadFrame.rightFrame.divisionTextBox.text()
+        if(UI_MainWindow.Ui_MainWindow.tab.UploadFrame.rightFrame.Dir.isChecked):
             SwaMe.Dir = True
 
-        SwaMePath = FileInput.BrowseWindow.GetSwaMePath(SwaMe)
         if  SwaMePath and SwaMe.File and SwaMe.Division!=5000 and SwaMe.MassTolerance!=5000:
-            rstring = "r"
-            QtCore.QProcess.startDetached(rstring.join(SwaMePath), 
-                ["-i" , SwaMe.File, "-d", SwaMe.Division, "-m", SwaMe.MassTolerance, "--dir", str(SwaMe.Dir)])
+            arguments = SwaMePath[0] + " -i " + SwaMe.File+ " --dir "+ str(SwaMe.Dir)+ " -d " +SwaMe.Division+ " -m " + SwaMe.MassTolerance
+
+        elif  SwaMePath and SwaMe.File and SwaMe.Division!=5000:
+            arguments = SwaMePath[0] + " -i " + SwaMe.File+ " --dir "+ str(SwaMe.Dir)+ " -d "+SwaMe.Division
+
+        elif  SwaMePath and SwaMe.File:
+            arguments = SwaMePath[0] + " -i " + SwaMe.File+ " --dir "+ str(SwaMe.Dir)
+            
+        SwaMe.process.start(arguments)
+
+    @QtCore.pyqtSlot()
+    def on_readyReadStandardOutput(self):
+        text = SwaMe.process.readAllStandardOutput().data().decode()
+        UI_MainWindow.Ui_MainWindow.tab.UploadFrame.rightFrame.textedit.append(text) 
 
 
 
+            
+        
