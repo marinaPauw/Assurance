73769fb1255101d26912c2a2aa9c71b0e4131814
Marina Pauw <pauwmarina@gmail.com>
2020-03-25 11:33:43 +0200

    RectangleSelector moved out of indMetrics and into RFSelectionPlots

--- IndividualMetrics.py
+++ IndividualMetrics.py
@@ -27,23 +27,8 @@ from cycler import cycler
 
 class MyIndMetricsCanvas(FigureCanvas):
     """Ultimately, this is a QWidget (as well as a FigureCanvasAgg, etc.)."""
-    def line_select_callback(eclick, erelease):
-        'eclick and erelease are the press and release events'
-        x1, y1 = eclick.xdata, eclick.ydata
-        x2, y2 = erelease.xdata, erelease.ydata
-        print("(%3.2f, %3.2f) --> (%3.2f, %3.2f)" % (x1, y1, x2, y2))
-        UI_MainWindow.Ui_MainWindow.predictionArea = [x1, y1, x2, y2]
-        UI_MainWindow.Ui_MainWindow.TrainingSet.goodbtn.setEnabled(True)
-        UI_MainWindow.Ui_MainWindow.TrainingSet.badbtn.setEnabled(True)
 
-    def toggle_selector(event):
-        print(' Key pressed.')
-        if event.key in ['Q', 'q'] and MyIndMetricsCanvas.toggle_selector.RS.active:
-            print(' RectangleSelector deactivated.')
-            MyIndMetricsCanvas.toggle_selector.RS.set_active(False)
-        if event.key in ['A', 'a'] and not MyIndMetricsCanvas.toggle_selector.RS.active:
-            print(' RectangleSelector activated.')
-            MyIndMetricsCanvas.toggle_selector.RS.set_active(True)
+
 
     
     def unique(list1): 
@@ -58,7 +43,7 @@ class MyIndMetricsCanvas(FigureCanvas):
 
         return unique_list
 
-    def __init__(self, tableContainingRownames, table,element, rectangleSelection,parent=None, width=25, height=8, dpi=100):
+    def __init__(self, tableContainingRownames, table,element, parent=None, width=25, height=8, dpi=100):
         try:
             if element == "StartTimeStamp":
                 table = UI_MainWindow.Ui_MainWindow.metrics[0]
@@ -157,16 +142,6 @@ class MyIndMetricsCanvas(FigureCanvas):
                                     QtWidgets.QSizePolicy.Expanding,
                                     QtWidgets.QSizePolicy.Expanding)
             FigureCanvas.updateGeometry(self)
-            # drawtype is 'box' or 'line' or 'none'
-            if(rectangleSelection):
-                MyIndMetricsCanvas.toggle_selector.RS = RectangleSelector(MyIndMetricsCanvas.ax,  MyIndMetricsCanvas.line_select_callback,
-                                            drawtype='box', useblit=True,
-                                            button=[1, 3],  # don't use middle button
-                                            minspanx=5, minspany=5,
-                                            spancoords='pixels',
-                                            interactive=True)
-                plt.connect('key_press_event', MyIndMetricsCanvas.toggle_selector)
-
             self.compute_initial_figure()
         except:
             if UI_MainWindow.Ui_MainWindow.metrics[0].columns.tolist().index(element) < len(UI_MainWindow.Ui_MainWindow.metrics[0].columns):
