81cb0210960c38d72b34debcb396c4d91391564f
Marina Pauw <pauwmarina@gmail.com>
2020-04-22 11:44:28 +0200

    Tidy up assurance imports

--- Assurance.py
+++ Assurance.py
@@ -1,31 +1,8 @@
 
 import sys
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-import math
-import sys
-import statistics
-import json
-import pandas as pd
-import numpy as np
-import numbers
-import math
-import matplotlib.pyplot as plt
-import statistics
-import scipy
-from sklearn import decomposition as sd
-from sklearn import preprocessing
-from sklearn.cluster import KMeans
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import RobustScaler
-from matplotlib.backends.backend_qt5agg import \
-    FigureCanvasQTAgg as FigureCanvas
-from matplotlib.figure import Figure
-import datetime
-import re
 import UI_MainWindow
+import PyQt5
+from PyQt5 import QtWidgets
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
