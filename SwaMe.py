bcec7ca8f706d7bf9024ddf0983832504c81a3da
Marina Pauw <pauwmarina@gmail.com>
2020-04-23 16:26:24 +0200

    SwaMe run bug fixes

--- SwaMe.py
+++ SwaMe.py
@@ -25,82 +25,113 @@ class SwaMe():
         
 
         #Actions for when buttons are clicked:
-        UI_MainWindow.Ui_MainWindow.tab.UploadFrame.rightFrame.SBrowseButton.clicked.connect(SwaMe.onSwaMeBrowseClicked)
-        UI_MainWindow.Ui_MainWindow.tab.UploadFrame.rightFrame.SRUNButton.clicked.connect(SwaMe.onSwaMeRUNClicked)
+        UI_MainWindow.Ui_MainWindow.SBrowseButton.clicked.connect(SwaMe.onSwaMeBrowseClicked)
+        UI_MainWindow.Ui_MainWindow.SRUNButton.clicked.connect(SwaMe.onSwaMeRUNClicked)
 
 
     def onSwaMeBrowseClicked(self):
         #FileInput.BrowseWindow.__init__(FileInput.BrowseWindow, self)
-        UI_MainWindow.Ui_MainWindow.tab.UploadFrame.leftFrame.files.show()
-        SwaMe.File = FileInput.BrowseWindow.GetSwaMeInputFile(SwaMe)
-        if(SwaMe.File):
-                fname = os.path.basename(SwaMe.File)
-                UI_MainWindow.Ui_MainWindow.tab.UploadFrame.rightFrame.SfileList.setText(fname)
+        UI_MainWindow.Ui_MainWindow.Sfiles.show()
+        SwaMe.Dir = FileInput.BrowseWindow.GetSwaMeInputFile(SwaMe)
+        if(SwaMe.Dir):
+                UI_MainWindow.Ui_MainWindow.SfileList.setText(SwaMe.Dir)
     
 
                 
     def onSwaMeRUNClicked(self):
-        SwaMePath = FileInput.BrowseWindow.GetSwaMePath(SwaMe)
-        directory = os.path.dirname(os.path.realpath(SwaMe.File))
-        dirpath = os.path.join(directory, "QC_Results")
-        if os.path.exists(dirpath):
-                timestr = time.strftime("%Y%m%d-%H%M%S")
-                ostr = timestr +"Older_QC_Results"
-                os.rename(dirpath,dirpath+timestr )
+        CurrentDir = os.getcwd()
+        os.chdir(SwaMe.Dir)
+        
+        SwaMe.Path = FileInput.BrowseWindow.GetSwaMePath(SwaMe)
+        SwaMe.timestr = time.strftime("%Y%m%d-%H%M%S")
            
-        SwaMe.process = QtCore.QProcess()
-        SwaMe.process.setProcessChannelMode(QtCore.QProcess.MergedChannels)
-        SwaMe.process.readyReadStandardOutput.connect(lambda: SwaMe.on_readyReadStandardOutput(self))
-        SwaMe.process.finished.connect(SwaMe.on_Finished)
-        arguments = ""
         
-        if(UI_MainWindow.Ui_MainWindow.tab.UploadFrame.rightFrame.MTTextBox.text()):
-            SwaMe.MassTolerance = arguments.join([" -m ", str(UI_MainWindow.Ui_MainWindow.tab.UploadFrame.rightFrame.MTTextBox.text())])
-        if(UI_MainWindow.Ui_MainWindow.tab.UploadFrame.rightFrame.RTTextBox.text()):
-            SwaMe.RTTolerance = arguments.join([" -rttolerance " , str(UI_MainWindow.Ui_MainWindow.tab.UploadFrame.rightFrame.RTTextBox.text())])
-        if(UI_MainWindow.Ui_MainWindow.tab.UploadFrame.rightFrame.divisionTextBox.text()):
-            SwaMe.Division = arguments.join([" -d " , str(UI_MainWindow.Ui_MainWindow.tab.UploadFrame.rightFrame.divisionTextBox.text())])
-        #if(UI_MainWindow.Ui_MainWindow.tab.UploadFrame.rightFrame.Dir.isChecked):
-        #    SwaMe.Dir = str.join(" -dir " , "true")
-        if(UI_MainWindow.Ui_MainWindow.IRTinputFile):
-            SwaMe.IRT = arguments.join([" -r " , str(UI_MainWindow.Ui_MainWindow.IRTinputFile)])
+       
+        #Find a file in the directory to be inputFile:
+        paths = []
+        files = []
+        with os.scandir(SwaMe.Dir) as entries:
+            for entry in entries:
+                if entry.name.endswith(".mzML"):
+                    paths.append(entry.path)
+                    files.append(entry.name)
+                    
+                    
+        SwaMe.StartProcess(self, files, 0, paths)
+                    
 
-        if  SwaMePath and SwaMe.File:
-            arguments = SwaMePath[0] + " -i " + SwaMe.File + SwaMe.Division + SwaMe.MassTolerance + SwaMe.RTTolerance + SwaMe.IRT  + " --dir true "
-
+    def StartProcess(self, files, file, paths):
+                    arguments = ""
+                    SwaMe.Division = ""
+                    SwaMe.MassTolerance =""
+                    SwaMe.RTTolerance = ""
+                    SwaMe.IRT = ""
         
-        SwaMe.process.start(arguments)
-        #SwaMe.process.waitForFinished();
-        #SwaMe.process.close();
-        #UI_MainWindow.Ui_MainWindow.tab.UploadFrame.rightFrame.textedit.append("closed.....")
+                    
+                    SwaMe.i = paths[file]
+                    SwaMe.filename = files[file]
+                    
+                    SwaMe.process = QtCore.QProcess()
+                    SwaMe.process.setProcessChannelMode(QtCore.QProcess.MergedChannels)
+                    SwaMe.process.readyReadStandardOutput.connect(lambda: SwaMe.on_readyReadStandardOutput(self))
+                    SwaMe.process.finished.connect(lambda: SwaMe.on_Finished(self, files, file, paths))
+        
+                    if len(SwaMe.i)<2:
+                        QtWidgets.QMessageBox.warning(self, "Warning","Does the folder contain mzML's?")
+                        SwaMe.onSwaMeBrowseClicked(self)
+                        SwaMe.onSwaMeRUNClicked(self)
+                    
+                    if(UI_MainWindow.Ui_MainWindow.MTTextBox.text()):
+                        SwaMe.MassTolerance = arguments.join([" -m ", str(UI_MainWindow.Ui_MainWindow.MTTextBox.text())])
+                    if(UI_MainWindow.Ui_MainWindow.RTTextBox.text()):
+                        SwaMe.RTTolerance = arguments.join([" -rttolerance " , str(UI_MainWindow.Ui_MainWindow.RTTextBox.text())])
+                    if(UI_MainWindow.Ui_MainWindow.divisionTextBox.text()):
+                        SwaMe.Division = arguments.join([" -d " , str(UI_MainWindow.Ui_MainWindow.divisionTextBox.text())])
+                    #if(UI_MainWindow.Ui_MainWindow.Dir.isChecked):
+                    #    SwaMe.Dir = str.join(" -dir " , "true")
+                    if(UI_MainWindow.Ui_MainWindow.IRTinputFile):
+                        SwaMe.IRT = arguments.join([" -r " , str(UI_MainWindow.Ui_MainWindow.IRTinputFile)])
+                     
+                    SwaMe.outputFile = " -o "+ str(SwaMe.timestr) +"_"+ os.path.splitext(os.path.basename(SwaMe.filename))[0] + ".json"
+
+                    if  SwaMe.Path and SwaMe.i:
+                        arguments = SwaMe.Path[0] + " -i " + SwaMe.i + SwaMe.Division + SwaMe.MassTolerance + SwaMe.RTTolerance + SwaMe.IRT  + SwaMe.outputFile
+
+                    SwaMe.process.start(arguments)
 
     @QtCore.pyqtSlot()
     def on_readyReadStandardOutput(self):
         text = SwaMe.process.readAllStandardOutput().data().decode()
-        UI_MainWindow.Ui_MainWindow.tab.UploadFrame.rightFrame.textedit.append(text)
+        UI_MainWindow.Ui_MainWindow.Stextedit.append(text)
         
     @QtCore.pyqtSlot()
-    def on_Finished(self):
-        dirpath = os.path.dirname(os.path.realpath(SwaMe.File))
-        dirpath = os.path.join(dirpath, "QC_Results", )
-        os.chdir(dirpath)
-        files = []
-        for root, dirs, allfiles in os.walk(dirpath):  
-            for file in allfiles:
-                if file.endswith(".json"):
-                    files.append(os.path.join(root,file))
-        UI_MainWindow.Ui_MainWindow.metrics = FileInput.BrowseWindow.CombineJSONs(UI_MainWindow.Ui_MainWindow, files)
-        #UI_MainWindow.Ui_MainWindow.metrics = FileInput.BrowseWindow.metricsParsing(inputFile)
-        #UI_MainWindow.Ui_MainWindow.checkColumnLength(self)
-        UI_MainWindow.Ui_MainWindow.NumericMetrics = []
-        FileInput.BrowseWindow.currentDataset = UI_MainWindow.Ui_MainWindow.metrics[0]
-        FileInput.BrowseWindow.currentDataset = DataPreparation.DataPrep.ExtractNumericColumns(UI_MainWindow.Ui_MainWindow,
-                           FileInput.BrowseWindow.currentDataset)
-        FileInput.BrowseWindow.currentDataset = DataPreparation.DataPrep.RemoveLowVarianceColumns(
-                           UI_MainWindow.Ui_MainWindow, FileInput.BrowseWindow.currentDataset)
-        UI_MainWindow.Ui_MainWindow.NumericMetrics.append(FileInput.BrowseWindow.currentDataset)
-        UI_MainWindow.Ui_MainWindow.DisableBrowseButtons(UI_MainWindow.Ui_MainWindow)
-        UI_MainWindow.Ui_MainWindow.EnableAnalysisButtons(UI_MainWindow.Ui_MainWindow)
+    def on_Finished(self, files, file, paths):
+        if SwaMe.filename ==files[-1]:#If the last file:
+                        
+            files = []
+            with os.scandir(SwaMe.Dir) as allfiles:
+                for file in allfiles:
+                    if file.name.endswith(".json"):
+                        if file.name.startswith(SwaMe.timestr):
+                            files.append(file.name)
+            UI_MainWindow.Ui_MainWindow.metrics = FileInput.BrowseWindow.CombineJSONs(UI_MainWindow.Ui_MainWindow, files)
+            #UI_MainWindow.Ui_MainWindow.metrics = FileInput.BrowseWindow.metricsParsing(inputFile)
+            #UI_MainWindow.Ui_MainWindow.checkColumnLength(self)
+            UI_MainWindow.Ui_MainWindow.NumericMetrics = []
+            FileInput.BrowseWindow.currentDataset = UI_MainWindow.Ui_MainWindow.metrics[0]
+            FileInput.BrowseWindow.currentDataset = DataPreparation.DataPrep.ExtractNumericColumns(UI_MainWindow.Ui_MainWindow,
+                            FileInput.BrowseWindow.currentDataset)
+            FileInput.BrowseWindow.currentDataset = DataPreparation.DataPrep.RemoveLowVarianceColumns(
+                            UI_MainWindow.Ui_MainWindow, FileInput.BrowseWindow.currentDataset)
+            UI_MainWindow.Ui_MainWindow.NumericMetrics.append(FileInput.BrowseWindow.currentDataset)
+            UI_MainWindow.Ui_MainWindow.DisableBrowseButtons(UI_MainWindow.Ui_MainWindow)
+            UI_MainWindow.Ui_MainWindow.EnableAnalysisButtons(UI_MainWindow.Ui_MainWindow)
+            QtWidgets.QMessageBox.about(self, "Message","SwaMe has finished successfully.")
+
+        else:
+            file = file+1
+            SwaMe.StartProcess(self, files, file, paths)
+                
         
 
             
